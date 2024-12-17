16dd88ac5277ab594bb2be83a890d7129ad852da
aharonkoss <aharonkoss@gmail.com>
2024-12-17 16:57:14 -0700

    Changes for 12.17.2024 landing page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createElement } from 'lwc';
 import App from 'x/app';
 
+
+
 const elm = createElement('x-app', { is: App });
 document.body.appendChild(elm);
--- src/modules/x/counter/counter.js
+++ src/modules/x/counter/counter.js
@@ -1,10 +1,7 @@
 import { LightningElement, api } from 'lwc';
-
 export default class extends LightningElement {
     @api label;
-
     counter = 0;
-
     increment() {
         this.counter++;
     }
