75131048eae578d545cfa768c11ec663f65875a9
aharonkoss <aharonkoss@gmail.com>
2024-12-19 19:35:33 -0700

    12-19-2024 last commit

--- src/modules/x/ornatePanel/ornatePanel.js
+++ src/modules/x/ornatePanel/ornatePanel.js
@@ -1,2 +1,20 @@
-import { LightningElement } from 'lwc';
-export default class OrnatePanel extends LightningElement {}
+import { LightningElement, track } from 'lwc';
+export default class OrnatePanel extends LightningElement {
+    @track isLeftPanelOpen=true;
+    
+    handleHotTopicsClick(event) {
+
+    }
+    handleGenerateDraft(event) {
+
+    }
+    get showHotTopics() {
+     //   return this.activeCategoryId && this.activeCategoryId !== 'salesperson-inputs' && this.activeCategoryId !== 'ai-research-summary';
+          return false;
+    }
+    get isSalespersonInputsActive() {
+        //return this.activeCategoryId === 'salesperson-inputs';
+        return false;
+    }
+    
+}
