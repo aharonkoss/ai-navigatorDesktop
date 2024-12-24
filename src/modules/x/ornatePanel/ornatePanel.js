c89247e41b9e82dfbce5db8bb072a302680919d7
aharonkoss <aharonkoss@gmail.com>
2024-12-24 07:26:03 -0700

    12-24-2-24: saved the combobox styling

--- src/modules/x/ornatePanel/ornatePanel.js
+++ src/modules/x/ornatePanel/ornatePanel.js
@@ -3,8 +3,8 @@ export default class OrnatePanel extends LightningElement {
     @track isLeftPanelOpen=true;
     @track isChecked=false;
     @track draftContent = '<p>Initial rich text content...</p>';
-    @track isSalespersonInputsActive=true;
-    @track showHotTopics=true;
+    @track _isSalespersonInputsActive=true;
+    @track _showHotTopics=true;
     @track isAIResearchSummaryActive=true;
     @track isChatPanelOpen=true;
     @track categories = [   
@@ -25,6 +25,10 @@ export default class OrnatePanel extends LightningElement {
             {label: 'Default', value: 'default'},
             {label: 'Data Driven', value: 'datadriven'}
     ];
+    @track clientTypeOptions=[
+        {label: 'Analytical', value: 'analytical'},
+        {label: 'Default', value: 'default'}
+];
     handleHotTopicsClick(event) {
         console.log('Hot Topics Click Function!');
         // Add your logic here
@@ -50,13 +54,16 @@ export default class OrnatePanel extends LightningElement {
     handleConversationStyleChange(event) {
         console.log('Selected Conversation Style:', event.detail.value);
     }
+    handleClientTypeChange(event) {
+        console.log('Selected Client Type:', event.detail.value);
+    }
     get showHotTopics() {
      //   return this.activeCategoryId && this.activeCategoryId !== 'salesperson-inputs' && this.activeCategoryId !== 'ai-research-summary';
-          return false;
+          return true;
     }
     get isSalespersonInputsActive() {
         //return this.activeCategoryId === 'salesperson-inputs';
-        return false;
+        return true;
     }
     get leftPanelClass() {
         return `left-panel ${this.isLeftPanelOpen ? 'open' : ''} ${this.isWidePanel ? 'wide' : ''}`;
