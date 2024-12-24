da04d2c890aabce2e9590eebdd7e08565e14171c
aharonkoss <aharonkoss@gmail.com>
2024-12-23 18:10:19 -0700

    12-23-2024: Check-in #2

--- src/modules/x/ornatePanel/ornatePanel.js
+++ src/modules/x/ornatePanel/ornatePanel.js
@@ -7,6 +7,24 @@ export default class OrnatePanel extends LightningElement {
     @track showHotTopics=true;
     @track isAIResearchSummaryActive=true;
     @track isChatPanelOpen=true;
+    @track categories = [   
+        { id: 'callplan', name: "Call Plan", completed: false, checkbox:false, checked : false },     
+        { id: 'salesperson-inputs', name: "Salesperson Inputs", completed: false, checkbox:false, checked : false },
+        { id: 'ai-research-summary', name: "AI Research Summary", completed: false, checkbox:false, checked : false },       
+        { id: 'build-trust', name: "Build Trust & Credibility", completed: false, checkbox: true, checked : false },
+        { id: 'frame-discussion', name: "Frame the Discussion", completed: false, checkbox: true, checked : false },
+        { id: 'explore-needs', name: "Explore Needs", completed: false, checkbox: true, checked : false },
+/*         { id: 'goals', name: "Goals", completed: false, checkbox: true, checked : false },
+        { id: 'obstacles', name: "Obstacles", completed: false, checkbox: true, checked : false },
+        { id: 'affect', name: "Affect", completed: false, checkbox: true, checked : false },
+        { id: 'logistics', name: "Logistics", completed: false, checkbox: true, checked : false }, */
+        { id: 'stories', name: "Stories", completed: false, checkbox: true, checked : false },
+        { id: 'joint-commitment', name: "Joint Commitment", completed: false, checkbox: true, checked : false }
+    ];
+    @track conversationOptions=[
+            {label: 'Default', value: 'default'},
+            {label: 'Data Driven', value: 'datadriven'}
+    ];
     handleHotTopicsClick(event) {
         console.log('Hot Topics Click Function!');
         // Add your logic here
@@ -29,6 +47,9 @@ export default class OrnatePanel extends LightningElement {
     handleDraftChange(event) {
         this.draftContent = event.detail.value;
     }
+    handleConversationStyleChange(event) {
+        console.log('Selected Conversation Style:', event.detail.value);
+    }
     get showHotTopics() {
      //   return this.activeCategoryId && this.activeCategoryId !== 'salesperson-inputs' && this.activeCategoryId !== 'ai-research-summary';
           return false;
@@ -39,6 +60,13 @@ export default class OrnatePanel extends LightningElement {
     }
     get leftPanelClass() {
         return `left-panel ${this.isLeftPanelOpen ? 'open' : ''} ${this.isWidePanel ? 'wide' : ''}`;
+    }
+    get categoriesWithClasses() {
+        return this.categories.map(category => ({
+            ...category,
+            itemClass:`category-item ${category.id == this.activeCategoryId ? ' background-antiquewhite' : ''}`,
+            iconClass: `category-icon ${category.completed ? 'completed' : ''}`
+        }));
     }
     
 }
